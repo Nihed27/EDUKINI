a8e7c156c91aed31a2fa65b2d74f7650e169edd7
Nihed27 <missarnihed167@gmail.com>
2026-04-01 22:58:08 +0100

    feat: Login et Register pages

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,20 @@
 import { Routes } from '@angular/router';
+import { Login } from './login/login';
+import { StatutEtudiant } from './statut-etudiant/statut-etudiant';
+import { DashboardEtudiant } from './dashboard-etudiant/dashboard-etudiant';
+import { DashboardAdmin } from './dashboard-admin/dashboard-admin';
+import { Enicarthage } from './enicarthage/enicarthage';
 import { EtudiantsComponent } from './etudiants/etudiants.component';
+import { Register } from './register/register';
+
 
 export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'register', component: Register },
+  { path: 'login', component: Login },
+  { path: 'statut', component: StatutEtudiant },
+  { path: 'dashboard-etudiant', component: DashboardEtudiant },
+  { path: 'dashboard-admin', component: DashboardAdmin },
+  { path: 'enicarthage', component: Enicarthage },
   { path: 'etudiants', component: EtudiantsComponent },
-  { path: '', redirectTo: 'etudiants', pathMatch: 'full' },
 ];
