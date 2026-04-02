874d9084a6b3cf397fbfd35b328a0ec3577c9910
Nihed27 <missarnihed167@gmail.com>
2026-04-02 21:46:34 +0100

    feat: add Spring Boot backend with H2 database

---
+++ backend/src/test/java/com/edukini/edukini_backend/EdukiniBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edukini.edukini_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdukiniBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
