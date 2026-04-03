57b607c3c0d2b8624f04cf5559731bc719206ed9
RachedILAHI <rached.ilahi@enicar.ucar.tn>
2026-04-03 21:18:35 +0100

    feat: ajout du composant Spécialités

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,11 @@
 import { Routes } from '@angular/router';
 import { FilieresComponent } from './filieres/filieres';
 import { SpecialitesComponent } from './specialites/specialites';
+import { DemandesComponent } from './demandes/demandes';
 
 export const routes: Routes = [
   { path: 'filieres',    component: FilieresComponent },
   { path: 'specialites', component: SpecialitesComponent },  // ← vérifie que c'est bien SpecialitesComponent
   { path: '', redirectTo: 'filieres', pathMatch: 'full' },
+  { path: 'demandes', component: DemandesComponent },
 ];
